8d031ddcd2a9c807f76ece234dc815d47dfca913
EDWARDCROCE1 <crazyedman4@yahoo.com>
2026-08-13 01:22:42 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
-const CACHE = 'asi-packfit-v2';
+const CACHE = 'asi-packfit-v2.1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './asi-logo.png',
-  './icon-192.png',
-  './icon-512.png',
-  './apple-touch-icon.png'
+  './', './index.html', './manifest.webmanifest', './asi-logo.png',
+  './icon-192.png', './icon-512.png', './apple-touch-icon.png'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
@@ -18,11 +13,7 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    const copy=response.clone(); caches.open(CACHE).then(cache => cache.put(event.request,copy)); return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
 });
